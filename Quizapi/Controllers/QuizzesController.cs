c7b57bbe09daeebc038f55ab9f83dc3a75304468
Ramprits <Rampritsahani@yahoo.com>
2017-12-10 20:28:29 +0530

    initial commite

---
+++ Quizapi/Controllers/QuizzesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quizapi.Models;
+
+namespace Quizapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Quizzes")]
+    public class QuizzesController : Controller
+    {
+        private readonly QuizDbContext _context;
+
+        public QuizzesController(QuizDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var getQuizz = _context.Quizzes.ToList();
+            return Ok(getQuizz);
+        }
+
+        [HttpGet("{Id}", Name = "GetQuiz")]
+        public IActionResult Get(int Id)
+        {
+            var getQuizz = _context.Quizzes.FirstOrDefault(x => x.ID == Id);
+            return Ok(getQuizz);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> PostQuiz([FromBody] Quiz quiz)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var OwnerId = HttpContext.User.Claims.First().Value;
+            quiz.OwnerId = OwnerId;
+            _context.Quizzes.Add(quiz);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetQuiz", new { id = quiz.ID }, quiz);
+        }
+
+    }
+}
---
+++ Quizapi/QuizDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quizapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizapi
+{
+    public class QuizDbContext : DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Quiz> Quizzes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
